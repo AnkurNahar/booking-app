442894b946118e3fd59836ccc3fa9213dbbe7f99
Ankur_Nahar <ankur.nahar@trucklagbe.com>
2023-04-03 10:39:06 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+
+func main(){
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50 
+
+	fmt.Println("Welcome to our", conferenceName, "booking application")
+	//printf -> format, %v to indicate place holder
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+
+	fmt.Printf("%v booked %v tickets.\n", userName, userTickets)
+}
